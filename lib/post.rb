a9c1e6008d4372c85cb6ae14f45f95e63de9f878
Mahendra Yadav <mahendra.k12@gmail.com>
2018-05-09 10:40:35 +0000

    automatically backed up by learn

--- lib/post.rb
+++ lib/post.rb
@@ -9,13 +9,9 @@ class Post
     @@all << self 
   end
   
- # def author_name
-#    if self.author 
-#      self.author.name 
-#    else 
-#      nil 
- #   end 
-#  end
+  def author_name
+    self.author.name
+  end
   
   def self.all
     @@all 
